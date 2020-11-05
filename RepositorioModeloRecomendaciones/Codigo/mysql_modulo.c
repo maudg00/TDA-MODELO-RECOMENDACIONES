913bc964dd5215ae708b6511c55e2049b23cc19c
Bern99Gr <70725770+Bern99Gr@users.noreply.github.com>
2020-11-05 16:53:03 -0700

    Add files via upload
    Propongo esto

---
+++ RepositorioModeloRecomendaciones/Codigo/mysql_modulo.c
@@ -0,0 +1,94 @@
+//
+//  mysql_modulo.c
+//
+//
+//  Created by Equipo 2 - 5/nov/2020
+//
+
+
+
+#include <stdlib.h>
+#include "mysql_modulo.h"
+
+char ** mysql_readquery(char query[], MYSQL* con)
+{
+    int fila;
+    int totalfilas=0;
+    char **resultados=NULL;
+    if (mysql_query(con, query))
+    {
+        finish_with_error(con);
+    }
+
+    MYSQL_RES *result = mysql_store_result(con);
+
+    if (result == NULL)
+    {
+        finish_with_error(con);
+    }
+    MYSQL_ROW row;
+    // Cuento el total de filas.
+    while ((row = mysql_fetch_row(result)))
+    {
+        totalfilas=totalfilas+1;
+        /*for(int i = 0; i < num_fields; i++)
+        {
+            printf("%s ", row[i] ? row[i] : "NULL");
+        }*/
+    }
+    mysql_free_result(result);
+
+    resultados=malloc(sizeof(char*)*totalfilas);
+    int i;
+    for(i=0; i<totalfilas;i++)
+    {
+        resultados[i]=malloc(sizeof(char)*100);
+    }
+
+    //Ahora obtengo resultados bien.
+    if (mysql_query(con, query))
+    {
+        finish_with_error(con);
+    }
+    MYSQL_RES *result2 = mysql_store_result(con);
+    if (result2 == NULL)
+    {
+        finish_with_error(con);
+    }
+    MYSQL_ROW row2;
+    // Cuento el total de filas.
+    i=0;
+    while ((row2 = mysql_fetch_row(result2)))
+    {
+        char string[100];
+        string[0]=0;
+        for(int j = 0; j < num_fields; j++)
+        {
+            if(j==0)
+            {
+                strcat(string, row[j]);
+            }
+            else
+            {
+                strcat(string, ",");
+                strcat(string, row[j]);
+            }
+        }
+        strcpy(resultados[i], string);
+        i+=1;
+    }
+    mysql_free_result(result2);
+    mysql_close(con);
+    return resultados;
+}
+
+MYSQL *con mysql_startconnection(){
+  MYSQL *mysql=NULL;;
+  mysql_init(mysql);
+  if(!mysql_real_connect(mysql, "localhost", "root", "", "tda", 0, NULL, 0))
+    {
+      printf("Error al conectarse a la base de datos %s\n", mysql_error(mysql));
+      exit(1);
+    }
+    return mysql;
+}
---
+++ RepositorioModeloRecomendaciones/Codigo/mysql_modulo.h
@@ -0,0 +1,50 @@
+//
+//  mysql_modulo.h
+//
+//
+//  Created by Equipo 2 - 5/nov/2020
+//
+
+#ifndef mysql_modulo_h
+#define mysql_modulo_h
+
+/*
+* System headers required by the following declarations
+* (the implementation will import its specific dependencies):
+*/
+
+#include <stdio.h>
+#include <mysql.h>
+
+/*
+* Application specific headers required by the following declarations
+* (the implementation will import its specific dependencies):
+*/
+
+
+/* Constants declarations. */
+
+/* Set EXTERN macro: */
+
+#ifndef mysql_modulo_IMPORT
+    #define EXTERN
+#else
+    #define EXTERN extern
+#endif
+
+
+/* Global variables declarations. */
+//
+//
+
+EXTERN char ** mysql_readquery(char query[], MYSQL con);
+EXTERN MYSQL *con mysql_startconnection();
+
+
+
+
+
+
+#undef mysql_modulo_IMPORT
+#undef EXTERN
+#endif /* mysql_modulo_h */
