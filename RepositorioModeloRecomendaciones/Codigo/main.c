670038c3936ff1ea04542c78bf2d9658dac617f1
maudg00 <70607519+maudg00@users.noreply.github.com>
2020-11-05 18:09:23 -0600

    Add files via upload

---
+++ RepositorioModeloRecomendaciones/Codigo/main.c
@@ -0,0 +1,11 @@
+#include "mysql_modulo.h"
+#include <mysql/mysql.h>
+int main()
+{
+    char **ResultadosQuery;
+    int filas=0;
+    MYSQL *con;
+    con=mysql_startconnection();
+    char query[]="SELECT u.id_usuario, u.edad, p.id_pelicula, p.titulo, u.genero, p.drama, p.accion, p.animacion, p.terror, p.comedia, p.G, p.PG13, p.R, p.actor_principal, p.casa_productora, p.duracion, p.anio_publicacion,  c.calificacion FROM usuarios u LEFT JOIN calificacion_peliculas c USING (id_usuario) LEFT JOIN peliculas p ON (p.id_peliculas=c.id_pelicula)";
+    ResultadosQuery=mysql_readquery(query, con, &filas);
+}
