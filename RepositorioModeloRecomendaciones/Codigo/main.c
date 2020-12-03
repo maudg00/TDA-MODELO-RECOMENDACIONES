418890d7cfee294b72f88cef44f57275ca020748
maudg00 <70607519+maudg00@users.noreply.github.com>
2020-12-02 22:28:40 -0600

    Update main.c

--- RepositorioModeloRecomendaciones/Codigo/main.c
+++ RepositorioModeloRecomendaciones/Codigo/main.c
@@ -1,6 +1,5 @@
 #include "mysql_modulo.h"
-
-#include <mysqlx/xapi.h>
+#include <mysql.h>
 //#include "datashell.h"
 #include "vista.h"
 /* Constants declarations. */
@@ -8,14 +7,12 @@
 #define FALSE 0
 int registrarUsuario(MYSQL* con);
 int loginUsuario(MYSQL* con, int *idUsuario, char * usuario);
-
 int main()
 {
     char **ResultadosQuery;
     int filas=0;
     MYSQL *con;
     con=mysql_startconnection();
-
     //char queryABT[]="SELECT u.id_usuario, u.edad, p.id_pelicula, p.titulo, u.genero, p.drama, p.accion, p.animacion, p.terror, p.comedia, p.G, p.PG13, p.R, p.actor_principal, p.casa_productora, p.duracion, p.anio_publicacion,  c.calificacion FROM usuarios u LEFT JOIN calificacion_peliculas c USING (id_usuario) LEFT JOIN peliculas p ON (p.id_peliculas=c.id_pelicula)";
     char usuario[27];
     int validar=FALSE;
@@ -53,8 +50,8 @@ int registrarUsuario(MYSQL* con)
         filas=0;
         vistaRegistro(usuario, contrasena, nombre, &genero, &edad);
         //Validar que no exista el usuario.
-        sprintf(queryLogin, "SELECT id_usuario FROM usuarios WHERE (username= '%s')", usuario);
-        resultados= mysql_readquery(queryLogin, con, &filas);
+        sprintf(queryRegistro, "SELECT id_usuario FROM usuarios WHERE (username= '%s')", usuario);
+        resultados= mysql_readquery(queryRegistro, con, &filas);
         if(filas!=0)
         {
             validarRegistro=FALSE;
@@ -96,4 +93,3 @@ int loginUsuario(MYSQL* con, int *idUsuario, char * usuario)
     sscanf(resultados[0],"%d", idUsuario);
     fflush(stdin);
 }
-
