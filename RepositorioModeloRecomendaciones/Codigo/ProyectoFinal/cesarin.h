b2a24a40802722068156c53a2d83e35b90e8d8d3
Bern99Gr <70725770+Bern99Gr@users.noreply.github.com>
2020-12-06 19:30:41 -0700

    Update cesarin.h

--- RepositorioModeloRecomendaciones/Codigo/ProyectoFinal/cesarin.h
+++ RepositorioModeloRecomendaciones/Codigo/ProyectoFinal/cesarin.h
@@ -26,6 +26,12 @@
 #endif
 
  EXTERN void cesarinObtenerAficiones(float **peliculas, float *calificaciones, size_t num_pelis, float *aficiones, int to_file);
+/*
+    La función cesarinObtenerAficiones manda a llamar a Stochastic Gradient Descent con los parámetros correspondientes para que 
+    pueda obtener en el arreglo de aficiones, las aficiones correspondientes del usuario.
+    @params float **peliculas, float *calificaciones, size_t num_pelis, float *aficiones, int to_file
+    @return 
+*/
 EXTERN void cesarinRecomendarPeliculas(float **peliculas, size_t num_pelis, float *aficiones, float *prediccion);
 /*
   Manda a llamar todo lo del modelo predictivo.
